7234a42261a85738577745c1d08a5ec579660e07
sopheedaire <alilinsofiadara@gmail.com>
2023-10-13 23:50:46 +0800

    Dashboard Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/Login';
+import DashboardScreen from './components/dashboard/dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <DashboardScreen />
     </div>
   );
 }
---
+++ src/components/dashboard/dashboard.tsx
@@ -0,0 +1,242 @@
+
+import header from '../../assets/headerlogo.png'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+import LogoutIcon from '@mui/icons-material/Logout';
+
+import HourglassBottomTwoToneIcon from '@mui/icons-material/HourglassBottomTwoTone';import BackHandOutlinedIcon from '@mui/icons-material/BackHandOutlined';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import BrokenImageOutlinedIcon from '@mui/icons-material/BrokenImageOutlined';
+
+import CircleIcon from '@mui/icons-material/Circle';
+
+import { useState } from 'react';
+function DashboardScreen() {
+    const [selectedItem, setSelectedItem] = useState('pending');
+    const handleButtonClick = (item: string) => {
+        setSelectedItem(item);
+      };
+    
+    
+    
+
+return (
+    <div className='DashboardContent'>
+        <div className="logocontainerD">
+        <img src={header} alt="Header Logo" className="mainlogoD" />
+        </div>
+
+        <div className="firstContent">
+            <div className="leftlabel">
+                <div className="iconContainer">
+                    <AccountCircleIcon/>
+                </div>
+                <div className="leftlabelstudent">
+                <div className="idNumber">
+                        2017100196
+                </div>
+                <div className="status">
+                    Status: <span>Cleared</span>
+                </div>
+                </div>
+            </div>
+            <div className="rightlabel">
+                        <LogoutIcon className='logoutIcon'/>
+                        <div className='rightText'> Logout </div>
+            </div>
+        </div>
+
+        <div className="dashboardLabel">
+            Dashboard
+        </div>
+
+
+        <div className="borrowItemsButton">
+            Click to Request Borrow Items
+        </div>
+
+        
+
+        {/* 4 buttons */}
+
+        <div className="buttonsView">
+            <button 
+                className={`buttonItemsStatus ${selectedItem === 'pending' ? 'button-active' : ''}`}
+                onClick={() => handleButtonClick('pending')}
+                
+            
+            >
+                <div className="iconStatus">
+                <HourglassBottomTwoToneIcon />
+                </div>
+                <div className="buttonName">Pending</div>
+            </button>
+
+            <button 
+                className={`buttonItemsStatus ${selectedItem === 'on-borrow' ? 'button-active' : ''}`}
+                
+                onClick={() => handleButtonClick('on-borrow')}
+                >
+                <div className="iconStatus">
+                <BackHandOutlinedIcon />
+                </div>
+                <div className="buttonName">On-Borrow</div>
+            </button>
+
+            <button 
+                className={`buttonItemsStatus ${selectedItem === 'return' ? 'button-active' : ''}`}
+                
+                onClick={() => handleButtonClick('return')}
+            >
+                <div className="iconStatus">
+                <ThumbUpOutlinedIcon />
+                </div>
+                <div className="buttonName">Return</div>
+            </button>
+
+            <button 
+                className={`buttonItemsStatus ${selectedItem === 'breakage' ? 'button-active' : ''}`}
+                
+                onClick={() => handleButtonClick('breakage')}
+            >
+                <div className="iconStatus">
+                <BrokenImageOutlinedIcon />
+                </div>
+                <div className="buttonName">Breakage</div>
+            </button>
+            </div>
+
+
+        {/* transaction containers */}
+        <div className="transactionView">
+        {selectedItem === 'pending' && (
+            <div className="transactionContainer">
+                <div className="transFirstRow">
+                    <div className="transactionID">
+                            Transaction ID #1002
+                    </div>
+                    <div className="currentStatus">
+                            <div className="iconCurrentStatus">
+                                <CircleIcon/>
+                            
+                           </div>
+                           <div className='penStatus'> Checking </div>
+                    </div>
+
+                </div>
+
+
+                <div className="transSecondRow">
+                        <div className="timeanddate">
+                        <div>August 19.2021</div> 
+                        <div>9:35 am</div>
+                        </div>
+
+                        <div className="pendingCancel">
+                            Cancel
+                        </div>
+
+                        
+                </div>
+                    
+                <div className="transThirdRow">
+                    Tap to View
+                </div>
+            </div>
+            )}
+        
+
+        {selectedItem === 'on-borrow' && (
+            <div className="transactionContainer">
+                <div className="transFirstRow">
+                    <div className="transactionID">
+                            TRANSACTION ID #1002
+                    </div>
+                    <div className="currentStatus">
+                            <div className="iconCurrentStatus">
+                            <CircleIcon className='obStatus'/>
+                            
+                           </div>
+                           <div className='obStatus'> Borrowing </div>
+
+                    </div>
+
+                </div>
+
+
+                <div className="transSecondRow">
+                        <div>August 19.2021</div> 
+                        <div>9:35 am</div>
+                </div>
+                    
+                <div className="transThirdRow">
+                    Tap to View
+                </div>
+            </div>
+            )}
+        
+        {selectedItem === 'return' && (
+            <div className="transactionContainer">
+                <div className="transFirstRow">
+                    <div className="transactionID">
+                            TRANSACTION ID #1002
+                    </div>
+                    <div className="currentStatus">
+                            <div className="iconCurrentStatus">
+                            <CircleIcon className='retStatus'/>
+                            
+                           </div>
+                           <div className='retStatus'> Completed  </div>
+
+                    </div>
+
+                </div>
+
+
+                <div className="transSecondRow">
+                        <div>August 19.2021</div> 
+                        <div>9:35 am</div>
+                </div>
+                    
+                <div className="transThirdRow">
+                    Tap to View
+                </div>
+            </div>
+            )}
+        {selectedItem === 'breakage' && (
+            <div className="transactionContainer">
+                <div className="transFirstRow">
+                    <div className="transactionID">
+                            TRANSACTION ID #1002
+                    </div>
+                    <div className="currentStatus">
+                            <div className="iconCurrentStatus">
+                            <CircleIcon className='breakStatus'/>
+                            
+                           </div>
+                           <div className='breakStatus'> Breakage  </div>
+
+                    </div>
+
+                </div>
+
+
+                <div className="transSecondRow">
+                        <div>August 19.2021</div> 
+                        <div>9:35 am</div>
+                </div>
+                    
+                <div className="transThirdRow">
+                    Tap to View
+                </div>
+            </div>
+            )}
+
+            
+        </div>
+
+    </div>
+);
+}
+
+export default DashboardScreen;
