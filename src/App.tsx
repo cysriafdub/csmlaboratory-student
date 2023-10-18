75e2720f6b861924c7bf755561bfbd753426f5c3
Pongot, Jophiel <jophiel.telen@gmail.com>
2023-10-18 14:34:57 +0800

    transaction peding view

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/Login';
 import DashboardScreen from './components/dashboard/dashboard';
+import PendingView from './components/transactionView/pendingView';
 
 function App() {
   return (
     <div className="App">
-      <DashboardScreen />
+      <PendingView />
     </div>
   );
 }
