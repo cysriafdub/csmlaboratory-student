6961b23241fe9d712960b7f53e1a5edf65aeccfa
sopheedaire <psofiadara@gmail.com>
2023-10-21 08:43:07 +0800

    additional views

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import './App.css';
 import Login from './components/login/Login';
 import DashboardScreen from './components/dashboard/dashboard';
 import PendingView from './components/transactionView/pendingView';
+import OnBorrowView from './components/transactionView/onBorrowView';
+import ReturnCompletedView from './components/transactionView/returnCompletedView';
+
 
 function App() {
   return (
     <div className="App">
-      <PendingView />
+      <ReturnCompletedView/>
     </div>
   );
 }
